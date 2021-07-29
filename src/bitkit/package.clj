3a48d0d7a9f166c6214db77317b5f8b61ed173a3
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-07-28 21:07:48 -0400

    make use of defrecord
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

--- src/bitkit/package.clj
+++ src/bitkit/package.clj
@@ -2,21 +2,52 @@
   (:require [babashka.process :refer [$ check]]
             [clojure.string :as string]))
 
+(defrecord RuntimeConfig [binary upgrade upgrade-system refresh install uninstall])
+(def brew (RuntimeConfig. "brew" "upgrade" "upgrade" "update" "install -f -q" "uninstall"))
+(def apt (RuntimeConfig. "apt" "install" "dist-upgrade" "update" "install -qyf" "remove -qyf"))
+
+(defn pkg-runtime-config [var]
+  (get-in runtime-config var))
+
+(defn- os-name
+  []
+  (string/lower-case (System/getProperty "os.name")))
+
+(defn- pkg-runtime
+  []
+  (condp #(string/includes? %2 %1) (os-name)
+    "linux" apt
+    "mac" brew
+    ))
+
 (defn install [pkgs]
   "install packages"
-  (-> ($ echo install ~(string/join " " pkgs))
+  (-> ($ ~(:binary (pkg-runtime)) ~(:install (pkg-runtime)) ~(string/join " " pkgs))
       check
       :out
       slurp))
 
-(defn uninstall []
-  (-> ($ echo uninstall)
+(defn uninstall [pkgs]
+  (-> ($ ~(pkg-runtime-prefix :action :uninstall) ~(string/join " " pkgs))
       check
       :out
       slurp))
 
 (defn refresh []
-  (-> ($ echo brew update)
+  (-> ($ ~(pkg-runtime-prefix :action :refresh))
+      check
+      :out
+      slurp))
+
+(defn upgrade [pkgs]
+  (-> ($ ~(pkg-runtime-prefix :action :upgrade) ~(string/join " " pkgs))
+      check
+      :out
+      slurp))
+
+
+(defn upgrade-system []
+  (-> ($ ~(pkg-runtime-prefix :action :upgrade-system))
       check
       :out
       slurp))
